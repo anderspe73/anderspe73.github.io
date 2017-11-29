b5bdfa2b78bd9d9f32f6e6bd7d02427575957bbd
anderspe73 <anderspe73@users.noreply.github.com>
2017-11-30 09:47:33 +1000

    Add files via upload

---
+++ files/bibtexbrowser.local.php
@@ -0,0 +1,7 @@
+<?php
+  include('IEEEStyleForBibtexbrowser.php');
+  define('BIBLIOGRAPHYSTYLE','IEEEStyle');
+//define('ABBRV_TYPE','none');
+//define('BIBTEXBROWSER_USE_PROGRESSIVE_ENHANCEMENT',false);
+?>
+ 
