1799233114c4a91ccd01276daad9811cb891b05e
Anders Eriksson <anderspe73@gmail.com>
2017-12-05 12:00:42 +1000

    a

--- README.md
+++
@@ -1,2 +0,0 @@
-# anderspe73.github.io
-Webpage
--- bibtexbrowser.local.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include('IEEEStyleForBibtexbrowser.php');
-  define('BIBLIOGRAPHYSTYLE','IEEEStyle');
-  define('ABBRV_TYPE','none');
-  define('BIBTEXBROWSER_DOI_LINKS',false);
-  ?>
-  
-  
